a912ac35094357320e4b7d86bdb5bdab18221db1
Artem Shibakov <shibakow@gmail.com>
2023-04-03 00:25:06 +0400

    fix: dirname es-modues style

--- src/config.js
+++ src/config.js
@@ -1,5 +1,9 @@
 import dotenv from "dotenv";
 import { resolve } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const result = dotenv.config({
   path: resolve(__dirname, "../.env"),
