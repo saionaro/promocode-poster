92625836b8df762ffc24a1d48d27e151b64439ec
Artem Shibakov <shibakow@gmail.com>
2023-04-03 00:15:33 +0400

    chore: add .env path

--- src/config.js
+++ src/config.js
@@ -1,6 +1,9 @@
 import dotenv from "dotenv";
+import { resolve } from "path";
 
-const result = dotenv.config();
+const result = dotenv.config({
+  path: resolve(__dirname, "../.env"),
+});
 
 if (result.error) {
   throw result.error;
