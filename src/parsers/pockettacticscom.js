8ed7bd1fbdee740831d0ff1ffff7d84426dd3935
Artem Shibakov <shibakow@gmail.com>
2024-01-20 18:14:32 +0400

    fix scrapping

--- src/parsers/pockettacticscom.js
+++ src/parsers/pockettacticscom.js
@@ -1,6 +1,11 @@
 import { parseJson, sanitize } from "../util.js";
 import { BaseParser } from "../base_parser.js";
 
+const LIST_SELECTORS = [
+  ".post-6906 ul",
+];
+const DIVIDER = " – ";
+
 export default class Parser extends BaseParser {
   constructor() {
     super();
@@ -10,10 +15,10 @@ export default class Parser extends BaseParser {
     const parsed = [];
     for (const r of rawList) {
       let val = sanitize(r);
-      const [code, ...description] = val.split(/ [-–] /);
+      const [code, ...description] = val.split(DIVIDER);
       parsed.push({
         code: code.toUpperCase(),
-        description: description.join(" - "),
+        description: description.join(DIVIDER),
         source: this.url,
       });
     }
@@ -21,14 +26,15 @@ export default class Parser extends BaseParser {
   }
   async getCodes() {
     const page = await this.getPage();
-    const codesUlSelector = ".post-6906 ul";
-    await page.waitForSelector(codesUlSelector);
+    const codesUlSelector = LIST_SELECTORS.join(", ");
+    await page.waitForSelector(".category-genshin-impact");
     const received = await page.evaluate((selector) => {
       const lists = document.querySelectorAll(selector);
-      const list = lists[1];
       const items = [];
-      for (const child of list.children) {
-        items.push(child.innerText);
+      for (const list of [...lists].slice(0, 1)) {
+        for (const child of list.children) {
+          items.push(child.innerText);
+        }
       }
       return JSON.stringify(items);
     }, codesUlSelector);
--- src/parsers/vg247com.js
+++ src/parsers/vg247com.js
@@ -1,6 +1,16 @@
 import { parseJson, sanitize } from "../util.js";
 import { BaseParser } from "../base_parser.js";
 
+const LIST_SELECTORS = [
+  "#codes + ul",
+  "#codes + p + ul",
+  "#livestream + ul",
+  "#livestream + p + ul",
+  "#livestream-codes + ul",
+  "#livestream-codes + p + ul",
+];
+const DIVIDER = ": ";
+
 export default class Parser extends BaseParser {
   constructor() {
     super();
@@ -10,10 +20,10 @@ export default class Parser extends BaseParser {
     const parsed = [];
     for (const r of rawList) {
       let val = sanitize(r);
-      const [code, ...description] = val.split(" - ");
+      const [code, ...description] = val.split(DIVIDER);
       parsed.push({
         code: code.toUpperCase(),
-        description: description.join(" - "),
+        description: description.join(DIVIDER).trim(),
         source: this.url,
       });
     }
@@ -21,7 +31,7 @@ export default class Parser extends BaseParser {
   }
   async getCodes() {
     const page = await this.getPage();
-    const codesUlSelector = ["#codes + ul", "#livestream + ul"].join(", ");
+    const codesUlSelector = LIST_SELECTORS.join(", ");
     await page.waitForSelector(".article_body_content");
     const received = await page.evaluate((selector) => {
       const lists = document.querySelectorAll(selector);
